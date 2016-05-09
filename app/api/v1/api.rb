4d4994706fd395ac2633ecaf894da75134381086
enixdark <cqshinn92@gmail.com>
2016-05-09 13:42:48 +0700

    update rename from and to field to from_email and to_email, add filter range for timestamp

--- app/api/v1/api.rb
+++ app/api/v1/api.rb
@@ -24,14 +24,15 @@ module V1
     # extract and format some field from json message
     def self.extract_message(item)
       message = item["message"]
-      timestamp = DateTime.parse("#{DateTime.parse(item['@timestamp']).year} \
-          #{REGEX_TIME.match(message)[0]}")
-      .strftime("%Y-%m-%d %H:%M:%S") rescue nil
+      # timestamp = DateTime.parse("#{DateTime.parse(item['@timestamp']).year} \
+      #     #{REGEX_TIME.match(message)[0]}")
+      # .strftime("%Y-%m-%d %H:%M:%S") rescue nil
+      timestamp =  DateTime.parse(item["@timestamp"]).strftime("%Y-%m-%d %H:%M:%S") rescue nil
       from = REGEX_EMAIL.match(REGEX_FROM_EMAIL.match(message)[0])[0] rescue nil
       to = REGEX_EMAIL.match(REGEX_TO_EMAIL.match(message)[0])[0] rescue nil
       status = REGEX_STATUS.match(message)[0].split("=")[1] rescue nil
       subject = REGEX_SUBJECT.match(message)[0].split(" ")[1..-2].join(" ") rescue nil
-      error_message = status == "deferred" ? ( REGEX_ERROR_MESSAGE.match(message) rescue nil )  : nil
+      error_message = status == "deferred" ? ( REGEX_ERROR_MESSAGE.match(message)[0] rescue nil )  : nil
       { :timestamp =>  timestamp,
         :status => status,
         :from => from ,
@@ -40,41 +41,45 @@ module V1
       }.merge( error_message ? { error_message: error_message } : {})
     end
 
-    def self.compare_time(time1, time2)
+    def self.compare_time(time1, time2, symbol)
       tp = DateTime.parse(time1) #.strftime('%Q')
       ti = DateTime.parse(time2)
-      DateTime.parse(tp.strftime('%Y-%m-%d')) <= tp && tp <= ti
+      symbol == :to ? tp <= ti : tp >= ti
     end
 
 
     resource :messages do
       desc 'Return result messages'
       params do
+        optional :from_email, type: String
+        optional :to_email, type: String
+        optional :status, type: String
         optional :from, type: String
         optional :to, type: String
-        optional :status, type: String
-        optional :timestamp, type: String
         optional :page, type: Integer
       end
-      get :filter do
+      get do
         # response = Elasticsearch::Model.client.perform_request 'POST', 'logstash-*/_search?size=
         page = params.key?(:page) ? params[:page].to_i : 1
-        shoulds = [] << {}.merge(params.slice(:from, :to, :status, :timestamp)).values.map do |item|
+        must = [] << {}.merge(params.slice(:from_email, :to_email, :status)).values.map do |item|
           {
             match: {
                 message: item
             }
           }
         end
-        query = shoulds.flatten.empty? ? { query: { match_all: {} } } : { query: { bool: { must: shoulds } } }
-        response = Elasticsearch::Model.client.search index: 'logstash-*', 
+        from = params.key?(:from) ? DateTime.parse("#{params[:from]} 00:00:00").strftime('%Q').to_i : nil
+        to = params.key?(:to) ? DateTime.parse("#{params[:to]} 23:59:59").strftime('%Q').to_i : nil
+        must[0] << { range: { "@timestamp": {}.merge(gte: from).merge(lte: to).select { |key,value| value != nil } } }
+        query = must.flatten.empty? ? { query: { match_all: {} } } : { query: { bool: { must: must } } }
+        response = Elasticsearch::Model.client.search index: 'logstash-*',
           body: query.merge( { from: (page - 1)*NUMBER_ON_PAGE, size: NUMBER_ON_PAGE })
-
         records = response["hits"]["hits"].map { |item| item["_source"] }
                                           .map { |item| API.extract_message(item) }
-                                          .select { |item| params.slice(:to,:from,:status,:timestamp).keys.map{ |it| (
-                                            it.to_sym != :timestamp ? ( params[it] == item[it.to_sym] )
-                                             : API.compare_time(params[it],item[it.to_sym]) ) }.all? }
+                                          .select do |item| params.slice(:from_email, :to_email, :to,:from,:status).keys.map do |it| (
+                                            it.to_sym != :to && it.to_sym != :from ? ( params[it] == item[it.to_sym] )
+                                             : API.compare_time(item[:timestamp],params[it], it.to_sym) ) end.all?
+                                          end
 
         # return json data
         {
